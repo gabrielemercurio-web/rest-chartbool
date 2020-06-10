528776c8d19c95d1818321c20483eb01a82875a6
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-06-11 00:17:58 +0200

    Milestone1: Secondo Grafico Completato

--- main.js
+++ main.js
@@ -8,16 +8,19 @@ $(document).ready(function() {
         'success': function(data) {
 
             var mesi = {};
+            var fatturati = {};
+            var fatturato_totale = 0;
 
             for (var i = 0; i < data.length; i++) {
                 var dati_venditore = data[i];
-                console.log(dati_venditore);
 
                 var data_corrente = dati_venditore.date;   // leggo la data di ogni oggetto
-                console.log(data_corrente);
 
                 var fatturato_corrente = dati_venditore.amount;
-                console.log(fatturato_corrente);
+                fatturato_totale += fatturato_corrente;
+
+                var venditore_corrente = dati_venditore.salesman;
+                console.log(venditore_corrente);
 
                 var data_moment = moment(data_corrente, 'DD/MM/YYYY');
 
@@ -33,19 +36,30 @@ $(document).ready(function() {
                 }
 
                 console.log(mesi);
+
+                if (!fatturati.hasOwnProperty(venditore_corrente)) {
+
+                    fatturati[venditore_corrente] = fatturato_corrente;
+
+                } else {
+
+                    fatturati[venditore_corrente] += fatturato_corrente;
+
+                }
+
+                console.log(fatturati);
             }
 
-            // estrapolo dall'oggeto 'mesi' il mese in lettere e il relativo fatturato totale
+            console.log(fatturato_totale);
 
+            // estrapolo dall'oggeto 'mesi' il mese in lettere e il relativo fatturato totale
             var mese = Object.keys(mesi);
-            console.log(mese);
             var fatturato = Object.values(mesi);
-            console.log(fatturato);
 
             // e li inserisco dentro labels e datasets
-
-            var ctx = $('#myChart')[0].getContext('2d');
-            var myChart = new Chart(ctx, {
+            // GRAFICO A LINEA
+            var ctx_1 = $('#ChartLine')[0].getContext('2d');
+            var chartLine = new Chart(ctx_1, {
                 type: 'line',
                 data: {
                     labels: mese,
@@ -68,6 +82,43 @@ $(document).ready(function() {
                         ],
                         borderColor: [
                             '#333'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            });
+
+
+
+            // estrapolo dall'oggeto 'fatturati' il nome del venditore e il relativo fatturato totale
+            var nome_venditore = Object.keys(fatturati);
+            var fatturati_venditori = Object.values(fatturati);
+
+            // calcolo il fatturato di ciascun venditore in %
+            var fatturato_percentuale = [];
+
+            for (var i = 0; i < fatturati_venditori.length; i++) {
+                fatturato_venditore_corrente = fatturati_venditori[i]
+
+                var fatturato_percentuale_corrente = (fatturato_venditore_corrente / fatturato_totale) * 100;
+
+                fatturato_percentuale.push(fatturato_percentuale_corrente.toFixed(1));
+            }
+
+            // GRAFICO A TORTA
+            var ctx_2 = $('#ChartPie')[0].getContext('2d');
+            var chartLine = new Chart(ctx_2, {
+                type: 'pie',
+                data: {
+                    labels: nome_venditore,
+                    datasets: [{
+                        label: 'Fatturato Mensile dei Venditori (%)',
+                        data: fatturato_percentuale,
+                        backgroundColor: [
+                            'rgba(255, 159, 164, 0.8)',
+                            'rgba(155, 115, 234, 0.8)',
+                            'rgba(215, 59, 64, 0.8)',
+                            'rgba(149, 219, 146, 0.8)',
                         ],
                         borderWidth: 1
                     }]
